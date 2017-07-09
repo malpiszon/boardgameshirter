97525de1f36821d00cc3615fe4022c50f6bb04a2
alan <alan@malpiszon.net>
2017-07-09 19:47:54 +0200

    Initial commit

---
+++ src/main/java/net/malpiszon/boardgameshirter/BoardgameshirterApplication.java
@@ -0,0 +1,12 @@
+package net.malpiszon.boardgameshirter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameshirterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgameshirterApplication.class, args);
+	}
+}
